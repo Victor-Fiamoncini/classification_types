2f2c4b8e499f7a58eb78e54f736c241479e069a9
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2021-07-10 19:54:36 -0300

    add hit rate

--- src/binary/01.py
+++ src/binary/01.py
@@ -11,17 +11,24 @@ dog03 = [0, 1, 1]
 
 mysterious_animal01 = [1, 1, 1]
 mysterious_animal02 = [1, 0, 0]
+mysterious_animal03 = [0, 0, 1]
 
 # 1 = pig & -1 = dog
-tags = [1, 1, 1, -1, -1, -1]
+train_tags = [1, 1, 1, -1, -1, -1]
 train_data = [pig01, pig02, pig03, dog01, dog02, dog03]
-
-# Data to predict (test)
-test = [mysterious_animal01, mysterious_animal02]
+test_tags = [-1, 1, 1]
+test_data = [mysterious_animal01, mysterious_animal02, mysterious_animal03]
 
 model = MultinomialNB()
-model.fit(train_data, tags)
+model.fit(train_data, train_tags)
 
-prediction = model.predict(test)
+prediction = model.predict(test_data)
 
-print(prediction)
+differences = prediction - test_tags
+
+hits = [d for d in differences if d == 0]
+total_hits = len(hits)
+total_test_elements = len(test_data)
+hits_rate = (100 * total_hits) / total_test_elements
+
+print(total_hits, total_test_elements, hits_rate)
