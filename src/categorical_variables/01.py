64f5b07c0291727f14f95700e872399e4ebad79b
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2021-07-11 18:18:37 -0300

    add dummy classifier

--- src/categorical_variables/01.py
+++ src/categorical_variables/01.py
@@ -1,4 +1,6 @@
+from collections import Counter
 from os import getcwd
+from sklearn.dummy import DummyClassifier
 from sklearn.naive_bayes import MultinomialNB
 import pandas as pd
 
@@ -24,17 +26,31 @@ y_train = y[:train_size]
 x_test = x[-test_size:]
 y_test = y[-test_size:]
 
+total_0s = len(y[y == 'sim'])
+total_1s = len(y[y == 'nao'])
+
+print('Total 0s:', total_0s)
+print('Total 1s:', total_1s)
+print('Number if most frequent value:', max(Counter(y).values()))
+
 ### Get predictions & hit rate
 model = MultinomialNB()
 model.fit(x_train, y_train)
 
 predictions = model.predict(x_test)
-differences = predictions - y_test
 
-hits = [d for d in differences if d == 0]
-total_hits = len(hits)
+hits = (predictions == y_test)
+
+total_hits = sum(hits)
 total_elements = len(x_test)
 
 hit_rate = 100 * total_hits / total_elements
 
-print(hit_rate, total_elements)
+print('MultinomialNB hit rate:', hit_rate)
+
+### Get most frequent Y (dummy strategy)
+dummy_model = DummyClassifier(strategy='most_frequent')
+dummy_model.fit(x_train, y_train)
+dummy_hit_rate = dummy_model.score(x_test, y_test) * 100
+
+print('DummyClassifier hit hate: %f' % dummy_hit_rate)
