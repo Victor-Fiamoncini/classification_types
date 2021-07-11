91befa789f97bcf5393a0e6a888e3a1df7e70035
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2021-07-11 19:33:06 -0300

    add user2 dataset

--- src/categorical_variables/01.py
+++ src/categorical_variables/01.py
@@ -1,11 +1,12 @@
 from collections import Counter
 from os import getcwd
+from sklearn.ensemble import AdaBoostClassifier
 from sklearn.dummy import DummyClassifier
 from sklearn.naive_bayes import MultinomialNB
 import pandas as pd
 
 ### Get data & separate train/test data
-file_path = getcwd() + '/src/categorical_variables/data/users.csv'
+file_path = getcwd() + '/src/categorical_variables/data/users2.csv'
 df = pd.read_csv(file_path)
 
 x_df = df[['home', 'busca', 'logado']]
@@ -26,15 +27,16 @@ y_train = y[:train_size]
 x_test = x[-test_size:]
 y_test = y[-test_size:]
 
-total_0s = len(y[y == 'sim'])
-total_1s = len(y[y == 'nao'])
+# total_0s = len(y[y == 'sim'])
+# total_1s = len(y[y == 'nao'])
 
-print('Total 0s:', total_0s)
-print('Total 1s:', total_1s)
+# print('Total 0s:', total_0s)
+# print('Total 1s:', total_1s)
 print('Number if most frequent value:', max(Counter(y).values()))
 
 ### Get predictions & hit rate
 model = MultinomialNB()
+model = AdaBoostClassifier()
 model.fit(x_train, y_train)
 
 predictions = model.predict(x_test)
