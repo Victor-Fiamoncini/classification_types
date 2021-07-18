57814f341bf23f1222133bdb21b86f47369e51c8
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2021-07-18 02:07:13 -0300

    feat: add fit and predict func

--- src/categorical_variables/01.py
+++ src/categorical_variables/01.py
@@ -6,7 +6,7 @@ from sklearn.naive_bayes import MultinomialNB
 import pandas as pd
 
 ### Get data & separate train/test data
-file_path = getcwd() + '/src/categorical_variables/data/users2.csv'
+file_path = getcwd() + '/src/categorical_variables/data/users.csv'
 df = pd.read_csv(file_path)
 
 x_df = df[['home', 'busca', 'logado']]
@@ -18,37 +18,47 @@ y_dummies_df = y_df
 x = x_dummies_df.values
 y = y_dummies_df.values
 
-train_size = int(0.9 * len(y))
-test_size = len(y) - train_size
+train_size = int(0.8 * len(y))
+test_size = int(0.1 * len(y))
+validation_size = len(y) - train_size - test_size
 
-x_train = x[:train_size]
-y_train = y[:train_size]
+x_train = x[0:train_size]
+y_train = y[0:train_size]
 
-x_test = x[-test_size:]
-y_test = y[-test_size:]
+x_test = x[train_size:train_size + test_size]
+y_test = y[train_size:train_size + test_size]
 
-# total_0s = len(y[y == 'sim'])
-# total_1s = len(y[y == 'nao'])
+x_validation = x[train_size + test_size:]
+y_validation = y[train_size + test_size:]
 
-# print('Total 0s:', total_0s)
-# print('Total 1s:', total_1s)
+total_0s = len(y[y == 'sim'])
+total_1s = len(y[y == 'nao'])
+
+print('Total 0s:', total_0s)
+print('Total 1s:', total_1s)
 print('Number if most frequent value:', max(Counter(y).values()))
 
 ### Get predictions & hit rate
+def fit_and_predict(model, x_train, y_train, x_test, y_test):
+  model.fit(x_train, y_train)
+
+  predictions = model.predict(x_test)
+  hits = (predictions == y_test)
+
+  total_hits = sum(hits)
+  total_elements = len(x_test)
+
+  hit_rate = 100 * total_hits / total_elements
+
+  print(type(model).__name__, 'hit rate:', hit_rate)
+
+  return hit_rate
+
 model = MultinomialNB()
+multinomial_hit_rate = fit_and_predict(model, x_train, y_train, x_test, y_test)
+
 model = AdaBoostClassifier()
-model.fit(x_train, y_train)
-
-predictions = model.predict(x_test)
-
-hits = (predictions == y_test)
-
-total_hits = sum(hits)
-total_elements = len(x_test)
-
-hit_rate = 100 * total_hits / total_elements
-
-print('MultinomialNB hit rate:', hit_rate)
+ada_boost_hit_rate = fit_and_predict(model, x_train, y_train, x_test, y_test)
 
 ### Get most frequent Y (dummy strategy)
 dummy_model = DummyClassifier(strategy='most_frequent')
