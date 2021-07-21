c36ebf96a32785605f0935896869bded6e1b6ced
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2021-07-21 20:45:07 -0300

    feat: add text to vector func

--- src/text_processing/01.py
+++ src/text_processing/01.py
@@ -1,24 +1,46 @@
 from os import getcwd
 import pandas as pd
-from past.builtins import xrange
 
-### Get data & separate train/test data
+### Getting data & converting emails to vectors
 file_path = getcwd() + '/src/text_processing/data/emails.csv'
 classifications = pd.read_csv(file_path)
 
 raw_emails = classifications['email']
 splitted_emails = raw_emails.str.lower().str.split(' ')
 
-all_words = set()
+words = set()
 for word_list in splitted_emails:
-  all_words.update(word_list)
+  words.update(word_list)
 
-all_words_total = len(all_words)
-all_words_tuples = list(zip(all_words, range(all_words_total)))
+words_total = len(words)
+words_tuples = zip(words, range(words_total))
+translator = {word:index for word, index in words_tuples}
+
+def text_to_vector(text, translator):
+  vector_phrase = [0] * len(translator)
+
+  for word in text:
+    if word in translator:
+      word_occurrence_index = translator[word]
+      vector_phrase[word_occurrence_index] += 1
+
+  return vector_phrase
+
+text_vectors = [text_to_vector(email, translator) for email in splitted_emails]
+
+### Train, Test & Validate data
+x = text_vectors
+y = classifications['classificacao']
+
+train_percentage = 0.8
+train_size = train_percentage * len(y)
+validation_size = len(y) - train_size
+
+x_train = x[0:train_size]
+y_train = y[0:train_size]
+
+x_validation = x[train_size:]
+y_validation = y[train_size:]
 
 
-email01 = "Se eu comprar cinco anos antecipados, eu ganho algum desconto?"
-email02 = "O exercício 15 do curso de Java 1 está com a resposta errada. Pode conferir pf?"
-email03 = "Existe algum curso para cuidar do marketing da minha empresa?"
-
-print(all_words_tuples, all_words_total)
+print(text_vectors)
